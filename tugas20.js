2b7a36d5b27c7ab9a7eb4cb7ae5beb4e4e50e55a
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 15:04:18 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+var kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+
+function panggilMatch(){
+	console.log(kalimat.match("are"))
+	console.log(kalimat.match("words"))
+}
+
+panggilMatch()
